e5a54b929b535670fc2e842cd067ffff491beec5
Omar Quispe <quispevargasomar@gmail.com>
2024-06-05 13:06:36 -0400

    store-api

---
+++ app/views/user_view.py
@@ -0,0 +1,20 @@
+def render_user_list(users):
+   # Representa una lista de animales como una lista de diccionarios
+   return [
+      {
+         "id": user.id,
+         "username": user.username,
+         "password": user.password_hash,
+         "roles": user.roles
+      }
+      for user in users
+   ]
+
+def render_user_detail(user):
+   # Representa los detalles de un animal como un diccionario
+   return {
+      "id": user.id,
+      "username": user.username,
+      "password": user.password_hash,
+      "roles": user.roles
+   }
